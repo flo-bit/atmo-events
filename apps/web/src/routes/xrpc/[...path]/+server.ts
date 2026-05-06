df4e91232f7a5168e275b445cc184ce10b287c3f
Florian <45694132+flo-bit@users.noreply.github.com>
2026-05-06 22:45:18 +0200

    add cors

--- apps/web/src/routes/xrpc/[...path]/+server.ts
+++ apps/web/src/routes/xrpc/[...path]/+server.ts
@@ -4,11 +4,27 @@ import type { RequestHandler } from './$types';
 
 const handle = createHandler(contrail);
 
+const CORS_HEADERS = {
+	'access-control-allow-origin': '*',
+	'access-control-allow-methods': 'GET, POST, OPTIONS',
+	'access-control-allow-headers': 'content-type',
+	'access-control-max-age': '86400'
+};
+
+function withCors(res: Response): Response {
+	const headers = new Headers(res.headers);
+	for (const [k, v] of Object.entries(CORS_HEADERS)) headers.set(k, v);
+	return new Response(res.body, { status: res.status, statusText: res.statusText, headers });
+}
+
 async function handler(request: Request, platform: App.Platform | undefined) {
 	const db = platform!.env.DB;
 	await ensureInit(db);
-	return handle(request, db) as Promise<Response>;
+	const res = (await handle(request, db)) as Response;
+	return withCors(res);
 }
 
 export const GET: RequestHandler = async ({ request, platform }) => handler(request, platform);
 export const POST: RequestHandler = async ({ request, platform }) => handler(request, platform);
+export const OPTIONS: RequestHandler = async () =>
+	new Response(null, { status: 204, headers: CORS_HEADERS });
